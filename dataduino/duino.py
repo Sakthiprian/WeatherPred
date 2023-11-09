e43c21a813772c7a4d2ed1615e90d3d3e0da9a94
supernovatux <thulashitharan.d@gmail.com>
2023-11-09 17:49:41 +0530

    Added arduino to python

---
+++ dataduino/duino.py
@@ -0,0 +1,11 @@
+import serial
+
+
+class duinodata:
+    def __init__(self, path: str):
+        self.ser = serial.Serial(path, 9600)
+        self.label = ('temperature', 'humidity', 'light')
+
+    def read(self):
+        self.ser.write(b'1')
+        return dict(zip(self.label, [float(i) for i in self.ser.readline().decode('utf-8').strip().split(':')]))
